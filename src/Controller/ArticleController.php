6c0a1d55373b15f888a98e96b7a10246ebcfeb72
Elisabeth Ngo <78852168+Eli-ngo@users.noreply.github.com>
2023-05-03 20:56:29 +0200

    add an article with all the checks

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -14,46 +14,72 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
 class ArticleController extends AbstractController
 {
-    //ADD AN ARTICLE
+    //AJOUT ARTICLE
     #[Route('/article', name: 'add_article', methods: ['POST'])]
     public function add(EntityManagerInterface $em, Request $r, Validator $v): Response
     {
-        $article = new Article();
+        //validation token
+        $headers = $r->headers->all();
 
-        $article->setTitle($r->get('title'));
+        if($headers['token'] != null && !empty($headers['token'])){
+            $jwt = current($headers['token']);
+            $key = $this->getParameter('jwt_secret');
 
-        $article->setContent($r->get('content'));
+            try{
+                $decoded = JWT::decode($jwt, new Key($key, 'HS256'));
+            }catch(\Exception $e){
+                return new JsonResponse($e->getMessage(), 403);
+            }
 
-        $article->setCreatedAt(new \DateTimeImmutable('now'));
+            //Validation du rôle
+            if($decoded->roles != null && in_array('ROLE_ADMIN', $decoded->roles)){
+                $article = new Article();
 
-        $article->setState($r->get('state'));
+                $article->setTitle($r->get('title'));
+        
+                $article->setContent($r->get('content'));
+        
+                $article->setCreatedAt(new \DateTimeImmutable('now'));
+        
+                $article->setState($r->get('state'));
+        
+                $article->setPublishmentDate(new \DateTimeImmutable($r->get('publishmentDate')));
+        
+                //on vérifie si l'utilisateur existe
+                $user = $em->getRepository(User::class)->findOneBy(['id' => $r->get('user')]);
+                if($user == null){
+                    return new JsonResponse('Utilisateur introuvable', 204);
+                }
+                $article->setAuthor($user);
+        
+                //on vérifie si la catégorie existe
+                $category = $em->getRepository(Category::class)->findOneBy(['id' => $r->get('category')]);
+                if($category == null){
+                    return new JsonResponse('Catégorie introuvable', 204);
+                }
+                $article->setCategory($category);
+        
+                $isValid = $v->isValid($article);
+                if($isValid !== true){
+                    return new JsonResponse($isValid, 400);
+                }
+    
+                $em->persist($article);
+                $em->flush();
+        
+                return new JsonResponse('Article ajouté', 200);
 
-        $article->setPublishmentDate(new \DateTimeImmutable($r->get('publishmentDate')));
-
-        //on vérifie si l'utilisateur existe
-        $user = $em->getRepository(User::class)->findOneBy(['id' => $r->get('user')]);
-        if($user == null){
-            return new JsonResponse('Utilisateur introuvable', 204);
+            }
+            return new JsonResponse('Vous ne disposez pas des droits pour ajouter un article', 403);
+            
         }
-        $article->setAuthor($user);
-
-        $category = $em->getRepository(Category::class)->findOneBy(['id' => $r->get('category')]);
-        if($category == null){
-            return new JsonResponse('Catégorie introuvable', 204);
-        }
-        $article->setCategory($category);
-
-        $isValid = $v->isValid($article);
-        if($isValid !== true){
-            return new JsonResponse($isValid, 400);
-        }
-
-        $em->persist($article);
-        $em->flush();
-
-        return new JsonResponse('Article ajouté', 200);
+        return new JsonResponse('Token invalide', 203);
 
     }
+    
 }
