f6719d9c7c957e93be372f81e5c25b49cbea3bf5
Elisabeth Ngo <78852168+Eli-ngo@users.noreply.github.com>
2023-05-03 14:31:50 +0200

    login with jwt security

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,14 +2,16 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+use Firebase\JWT\JWT;
 
 class UserController extends AbstractController
 {
@@ -25,16 +27,25 @@ class UserController extends AbstractController
     #[Route('/login', name: 'app_login', methods: ['POST'])]
     public function login (Request $r, EntityManagerInterface $em, UserPasswordHasherInterface $uph): Response
     {
-        $user = $em->getRepository(User::class)->findOneBy(['email' => $r->get('email')]);
+        $user = $em ->getRepository(User::class)->findOneBy(['email' => $r->get('email')]);
 
-        if($user == null){
+        if($user === null){
             return new JsonResponse('Utilisateur introuvable', 404);
         }
 
-        if($r->get('pwd') == null || !$uph->isPasswordValid($user, $r->get('pwd'))){
-            return new JsonResponse('Mot de passe érroné', 400);
+        if($r->get('pwd') == null || !$uph -> isPasswordValid($user, $r->get('pwd'))){
+            return new JsonResponse('Mot de passe incorrect', 400);
         }
 
-        return new JsonResponse('Vous êtes connecté', 200);
+        $key = $this->getParameter('jwt_secret');
+        $payload = [
+            'iat' => time(),
+            'exp' => time() + 3600,
+            'roles' => $user->getRoles(),
+        ];
+
+        $jwt = JWT::encode($payload, $key, 'HS256');
+
+        return new JsonResponse($jwt, 200);
     }
 }
