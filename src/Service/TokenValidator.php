f28b0353177820d129f15b64caa9d82820a3426f
Forofoot <erwan.marques@yahoo.fr>
2023-05-04 11:12:48 +0200

    Service token

--- src/Service/TokenValidator.php
+++ src/Service/TokenValidator.php
@@ -2,6 +2,41 @@
 
 namespace App\Service;
 
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
 class TokenValidator{
+
+    /**
+     * @var string
+     */
+    private $jwt_secret;
+
+    public function __construct(string $jwt_secret){
+        $this->jwt_secret = $jwt_secret;
+    }
+
+
+    public function checkToken($header)
+    {
+        if($header['token'] != null && !empty($header['token'])){
+            $token = $header['token'];
+            $jwt = current($token);
+            $key = $this->jwt_secret;
+            
+            try{
+                $decoded = JWT::decode($jwt, new Key($key, 'HS256'));
+            }catch(\Exception $e){
+                return new JsonResponse($e->getMessage(), 401);
+            }
+
+            if($decoded->roles != null && in_array('ROLE_ADMIN', $decoded->roles)){
+                return true;
+            }
+        }else{
+            return new JsonResponse('Token not found', 404);
+        }
+    }
 
 }
